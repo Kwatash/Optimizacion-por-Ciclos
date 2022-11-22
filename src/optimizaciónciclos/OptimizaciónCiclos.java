71ffb50a37d0092702699aa430a6d725382e39d5
bko <bko@DESKTOP-NIMHIF7>
2022-11-22 17:34:35 -0600

    Programa 2 Optimizacion Ciclos

---
+++ src/optimizaciónciclos/OptimizaciónCiclos.java
@@ -0,0 +1,8 @@
+package optimizaciónciclos;
+public class OptimizaciónCiclos {
+    public static void main(String[] args) {
+        for(int i = 500; i<=1000; i+=2){
+            System.out.println(i);
+        }
+    }
+}
